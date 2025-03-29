ab2411a6003e6d78b5380f53f16f5073a9e68d76
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-03-29 17:51:32 +0000

    feat: add uTest to validateGuess

--- gordle/game_internal_test.go
+++ gordle/game_internal_test.go
@@ -1,6 +1,7 @@
 package gordle
 
 import (
+	"errors"
 	"slices"
 	"strings"
 	"testing"
@@ -36,6 +37,37 @@ func TestGameAsk(t *testing.T) {
 			got := g.ask()
 			if !slices.Equal(got, tc.want) {
 				t.Errorf("got = %v, want %v", string(got), string(tc.want))
+			}
+		})
+	}
+}
+
+func TestGameValidateGuess(t *testing.T) {
+	tt := map[string]struct {
+		word     []rune
+		expected error
+	}{
+		"nominal": {
+			word:     []rune("GUESS"),
+			expected: nil,
+		},
+		"too short": {
+			word:     []rune("HI"),
+			expected: errInvalidWordLength,
+		},
+		"too long": {
+			word:     []rune("SHOULDFAIL"),
+			expected: errInvalidWordLength,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := New(nil)
+
+			err := g.validateGuess(tc.word)
+			if !errors.Is(err, tc.expected) {
+				t.Errorf("%c, expected %q, got %q", tc.word, tc.expected, err)
 			}
 		})
 	}
