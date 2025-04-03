55b0c82467eaed952c6b4f58c8828ae97f743ff7
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-04-03 01:47:45 +0100

    refactor: update main() to reflect changes in New()
    Add error handling on main()

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"gordle/gordle"
 	"os"
 )
 
 const maxAttempts = 6
+const corpusFile = "./corpus/engish.txt"
 
 func main() {
-	solution := "hello"
-	g := gordle.New(os.Stdin, solution, maxAttempts)
+	corpus, err := gordle.ReadCorpus(corpusFile)
+
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to read corpus: %s", err)
+		return
+	}
+
+	// create a new game
+	g, err := gordle.New(bufio.NewReader(os.Stdin), corpus, maxAttempts)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "uanble to start game: %s", err)
+		return
+	}
 
 	g.Play()
 }
